d2e579b0c87446fd11a07e1e79376a5e083b8fa4
Maria <cose@list.ru>
2023-06-24 21:22:04 +0300

    add Task

---
+++ src/main/java/ru/netology/Game.java
@@ -0,0 +1,39 @@
+package ru.netology;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+
+    List<Player> listOfPlayers = new ArrayList<>();
+
+    public void register(Player player) {
+        listOfPlayers.add(player);
+    }
+
+    public int round(String name1, String name2) {
+        Player player1 = null;
+        Player player2 = null;
+        for (Player player : listOfPlayers) {
+            if (player.getName().equals(name1)) {
+                player1 = player;
+            }
+            if (player.getName().equals(name2)) {
+                player2 = player;
+            }
+        }
+        if (player1 == null) {
+            throw new NotRegisteredException(name1);
+        }
+        if (player2 == null) {
+            throw new NotRegisteredException(name2);
+        }
+        if (player1.getStrength() > player2.getStrength()) {
+            return 1;
+        }
+        if (player1.getStrength() < player2.getStrength()) {
+            return -1;
+        }
+        return 0;
+    }
+}
---
+++ src/test/java/ru/netology/GameTest.java
@@ -0,0 +1,70 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class GameTest {
+
+    Player player1 = new Player(1, "player1", 10);
+    Player player2 = new Player(2, "player2", 30);
+    Player player3 = new Player(3, "player3", 50);
+    Player player4 = new Player(4, "player4", 20);
+    Player player5 = new Player(5, "player5", 50);
+    Player player6 = new Player(6, "player6", 40);
+
+    Game game = new Game();
+
+    @Test
+    public void FirstPlayerWinBothPlayersRegistered() {
+        game.register(player2);
+        game.register(player1);
+
+        int expected = 1;
+        int actual = game.round("player2", "player1");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void SecondPlayerWinsBothPlayersRegistered() {
+        game.register(player4);
+        game.register(player6);
+
+        int expected = -1;
+        int actual = game.round("player4", "player6");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void drawBothPlayersRegistered() {
+        game.register(player3);
+        game.register(player5);
+
+        int expected = 0;
+        int actual = game.round("player3", "player5");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void firstPlayerNotRegistered() {
+        game.register(player2);
+
+        Assertions.assertThrows(NotRegisteredException.class, ()-> {game.round("player1", "player2");});
+    }
+
+    @Test
+    public void TheSecondPlayerNotRegistered() {
+        game.register(player3);
+
+        Assertions.assertThrows(NotRegisteredException.class, ()-> {game.round("player3", "player4");});
+    }
+
+    @Test
+    public void BothPlayersAreNotRegistered() {
+
+
+        Assertions.assertThrows(NotRegisteredException.class, ()-> {game.round("player5", "player6");});
+    }
+}
